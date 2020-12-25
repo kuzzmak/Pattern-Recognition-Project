4351debaec5d5d9651e661c51dc0b016cfd8e150
kuzzmak <ak50380@fer.hr>
2020-12-25 01:25:16 +0100

    updated optical flow

--- testing_optical_flow.py
+++ testing_optical_flow.py
@@ -38,12 +38,11 @@ color = np.random.randint(0, 255, (100, 3))
 #         # cv.waitKey(0)
 
 
-def nearest_square(current: tuple, square_size: int) -> Tuple[tuple, tuple]:
+def nearest_square(current: tuple) -> Tuple[tuple, tuple]:
     """
     Finds square area in which current pixel belongs to.
 
     :param current: pixel for which is area being searched for
-    :param square_size: size of the square area
     :return: (y1, x1), (y2, x2) -> bounds of the square area
     """
     y_bound_up = int(square_size * np.ceil(current[0] / square_size))
@@ -54,7 +53,7 @@ def nearest_square(current: tuple, square_size: int) -> Tuple[tuple, tuple]:
 
 
 def add_square(svois, square_size, frame, dot):
-    nearest = nearest_square(np.flipud(dot).ravel(), square_size)
+    nearest = nearest_square(np.flipud(dot).ravel())
     # upper left and lower right corner of the square area
     p1, p2 = nearest
     square_from_image = frame[p1[0]:p2[0], p1[1]:p2[1]]
@@ -134,24 +133,72 @@ def add_square(svois, square_size, frame, dot):
 #                 svois_of_interest[k] = v
 #         yield svois_of_interest
 
+def draw_str(dst, text):
+    font = cv.FONT_HERSHEY_SIMPLEX
+    font_scale = 0.5
+    text_size = cv.getTextSize(text, font, font_scale, 2)[0]
+    text_x = int((dst.shape[1] - text_size[0]) / 2)
+    # text_y = int((dst.shape[0] + text_size[1]) / 2)
+    text_y = 15
+    cv.putText(dst, text, (text_x, text_y), font, font_scale, (255, 255, 255), 2)
+
+
+def fun(incidence_of_squares, seg):
+    for i in range(step_y):
+        for j in range(step_x):
+
+            p1 = (ii * square_size, jj * square_size)
+            p2 = ((ii + 1) * square_size, (jj + 1) * square_size)
+
+            im_array = seg[p1[0]:p2[0], p1[1]:p2[1]]
+
+            greater = np.count_nonzero(im_array > 0)
+            if greater >= total_pixels_in_square * 0.5:
+                square = (p1, p2)
+                if square not in incidence_of_squares0:
+                    incidence_of_squares[square] = 1
+                else:
+                    incidence_of_squares[square] += 1
+    pass
+
 path = os.path.join('data', 'UCSD_Anomaly_Dataset.v1p2', 'UCSDped1', 'Train', 'Train001')
 images = [x for x in os.listdir(path) if x.endswith(".tif")]
-first_frame = cv.imread(os.path.join(path, images[0]))
-# first_frame = first_frame[0:15, 0:15, :]
-prev_gray = cv.cvtColor(first_frame, cv.COLOR_BGR2GRAY)
+prev_gray = cv.imread(os.path.join(path, images[0]))
 
-##############################################3
+#############################
 # Farneback
-#############################33
+#############################
+
 # Creates an image filled with zero intensities with the same dimensions as the frame
-mask = np.zeros_like(first_frame)
+mask = np.zeros_like(prev_gray)
 # Sets image saturation to maximum
 mask[..., 1] = 255
+# length of the SVOI in time dimension
 temporal_length = 7
+# length of the SVOI square
+square_size = 15
+# total pixels in one frame of a SVOI
+total_pixels_in_square = square_size * square_size
+# pixel intensities greater or equal to sigma must be in
+# newly made rgb image which shows deep optical flow
+sigma = 10
+# percent of pixels in SVOI square that need to change
+# (from one frame to another) to be considered useful
+p_s = 0.5
+# going through all images by step of temporal_length
 for i in range(0, len(images), temporal_length):
+    if i + temporal_length > len(images):
+        break
+
     prev_path = os.path.join(path, images[i])
-    prev_gray = cv.imread(prev_path)
-    for j in range(temporal_length):
+    prev_gray = cv.imread(prev_path, 0)
+
+    incidence_of_squares0 = {}
+    incidence_of_squares1 = {}
+    incidence_of_squares2 = {}
+
+    for j in range(1, temporal_length):
+
         gray_path = os.path.join(path, images[i + j])
         gray = cv.imread(gray_path, 0)
 
@@ -161,6 +208,124 @@ for i in range(0, len(images), temporal_length):
         mask[..., 2] = cv.normalize(magnitude, None, 0, 255, cv.NORM_MINMAX)
         rgb_new = cv.cvtColor(mask, cv.COLOR_HSV2BGR)
 
+        # pixels that satisfy condition, pixels which intensities
+        # are greater than some threshold
+        satisfies = rgb_new >= sigma
+        seg = np.zeros_like(rgb_new)
+        # array like satisfies which has 255 intensity on all
+        # the places which satisfy threshold condition
+        seg[satisfies] = 255
+
+        gray_copy0 = gray.copy()
+        gray_copy1 = gray.copy()
+        gray_copy2 = gray.copy()
+
+        pic_shape1 = gray_copy0.shape
+        step_x = int(pic_shape1[1] / square_size)
+        step_y = int(pic_shape1[0] / square_size)
+        test0 = seg[:, :, 0]
+        test1 = seg[:, :, 1]
+        test2 = seg[:, :, 2]
+
+        for ii in range(step_y):
+            for jj in range(step_x):
+
+                p1 = (ii * square_size, jj * square_size)
+                p2 = ((ii + 1) * square_size, (jj + 1) * square_size)
+
+                im_array0 = test0[p1[0]:p2[0], p1[1]:p2[1]]
+                im_array1 = test1[p1[0]:p2[0], p1[1]:p2[1]]
+                im_array2 = test2[p1[0]:p2[0], p1[1]:p2[1]]
+
+                greater = np.count_nonzero(im_array0 > 0)
+                if greater >= total_pixels_in_square * 0.5:
+                    gray_copy0 = cv.rectangle(gray_copy0, (p1[1], p1[0]), (p2[1], p2[0]), (255, 0, 0), 1)
+                    square = (p1, p2)
+                    if square not in incidence_of_squares0:
+                        incidence_of_squares0[square] = 1
+                    else:
+                        incidence_of_squares0[square] += 1
+
+                greater = np.count_nonzero(im_array1 > 0)
+                if greater >= total_pixels_in_square * 0.5:
+                    gray_copy1 = cv.rectangle(gray_copy1, (p1[1], p1[0]), (p2[1], p2[0]), (255, 0, 0), 1)
+                    square = (p1, p2)
+                    if square not in incidence_of_squares1:
+                        incidence_of_squares1[square] = 1
+                    else:
+                        incidence_of_squares1[square] += 1
+
+                greater = np.count_nonzero(im_array2 > 0)
+                if greater >= total_pixels_in_square * 0.5:
+                    gray_copy2 = cv.rectangle(gray_copy2, (p1[1], p1[0]), (p2[1], p2[0]), (255, 0, 0), 1)
+                    square = (p1, p2)
+                    if square not in incidence_of_squares2:
+                        incidence_of_squares2[square] = 1
+                    else:
+                        incidence_of_squares2[square] += 1
+
+        grey_3_channel = cv.cvtColor(gray, cv.COLOR_GRAY2BGR)
+        gray_copy_3_channel0 = cv.cvtColor(gray_copy0, cv.COLOR_GRAY2BGR)
+        gray_copy_3_channel1 = cv.cvtColor(gray_copy1, cv.COLOR_GRAY2BGR)
+        gray_copy_3_channel2 = cv.cvtColor(gray_copy2, cv.COLOR_GRAY2BGR)
+        # drawing frame number
+        draw_str(grey_3_channel, f'frame: {i + j + 1}')
+
+        pics_to_show = (rgb_new, grey_3_channel, gray_copy_3_channel0, gray_copy_3_channel1, gray_copy_3_channel2)
+        numpy_horizontal = np.hstack(pics_to_show)
+        cv.imshow('imgs', numpy_horizontal)
+        # cv.waitKey(0)
+
+        prev_gray = gray
+
+    incidence_of_squares_sum = {}
+
+    prev_gray_copy0 = prev_gray.copy()
+    prev_gray_copy1 = prev_gray.copy()
+    prev_gray_copy2 = prev_gray.copy()
+    num_of_squares = 3
+    for k, v in incidence_of_squares0.items():
+        p1, p2 = k
+        if v >= num_of_squares:
+            if k not in incidence_of_squares_sum:
+                incidence_of_squares_sum[k] = 1
+            else:
+                incidence_of_squares_sum[k] += 1
+
+            cv.rectangle(prev_gray_copy0, (p1[1], p1[0]), (p2[1], p2[0]), (255, 0, 0), 1)
+
+    for k, v in incidence_of_squares1.items():
+        p1, p2 = k
+        if v >= num_of_squares:
+            if k not in incidence_of_squares_sum:
+                incidence_of_squares_sum[k] = 1
+            else:
+                incidence_of_squares_sum[k] += 1
+            cv.rectangle(prev_gray_copy1, (p1[1], p1[0]), (p2[1], p2[0]), (255, 0, 0), 1)
+
+    for k, v in incidence_of_squares2.items():
+        p1, p2 = k
+        if v >= num_of_squares:
+            if k not in incidence_of_squares_sum:
+                incidence_of_squares_sum[k] = 1
+            else:
+                incidence_of_squares_sum[k] += 1
+            cv.rectangle(prev_gray_copy2, (p1[1], p1[0]), (p2[1], p2[0]), (255, 0, 0), 1)
+
+    prev_gray_copy_sum = prev_gray.copy()
+    for k, v in incidence_of_squares_sum.items():
+        p1, p2 = k
+        if v >= 2:
+            cv.rectangle(prev_gray_copy_sum, (p1[1], p1[0]), (p2[1], p2[0]), (255, 0, 0), 1)
+
+    draw_str(prev_gray_copy0, 'seg0-squares')
+    draw_str(prev_gray_copy1, 'seg1-squares')
+    draw_str(prev_gray_copy2, 'seg2-squares')
+    draw_str(prev_gray_copy_sum, 'sum')
+    pics_to_show = (prev_gray_copy0, prev_gray_copy1, prev_gray_copy2, prev_gray_copy_sum)
+    numpy_horizontal = np.hstack(pics_to_show)
+    cv.imshow('squares', numpy_horizontal)
+    cv.waitKey(0)
     # frame = cv.imread(os.path.join(path, images[i]))
     # gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     # flow = cv.calcOpticalFlowFarneback(prev_gray, gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
@@ -174,19 +339,17 @@ for i in range(0, len(images), temporal_length):
     # satisfies = rgb_new >= 15
     # seg = np.zeros_like(rgb_new)
     # seg[satisfies] = 255
-    #
-    #
-    #
+
     # grey_3_channel = cv.cvtColor(gray, cv.COLOR_GRAY2BGR)
-    # # numpy_horizontal = np.hstack((rgb_new, grey_3_channel))
-    #
+    # numpy_horizontal = np.hstack((rgb_new, grey_3_channel))
+
     # rgb_old = rgb_new
-    # # numpy_vertical_concat = np.concatenate((image, grey_3_channel), axis=0)
-    # # numpy_horizontal_concat = np.concatenate((image, grey_3_channel), axis=1)
+    # numpy_vertical_concat = np.concatenate((image, grey_3_channel), axis=0)
+    # numpy_horizontal_concat = np.concatenate((image, grey_3_channel), axis=1)
     # maximums = np.amax(seg, axis=2)
-    # # cv2.imshow('Main', image)
-    # # cv2.imshow('Numpy Vertical', numpy_vertical)
-    #
+    # cv2.imshow('Main', image)
+    # cv2.imshow('Numpy Vertical', numpy_vertical)
+
     # gray_copy = gray.copy()
     # x, y = 15, 15
     # pic_shape1 = gray_copy.shape
@@ -210,14 +373,13 @@ for i in range(0, len(images), temporal_length):
     #
     # numpy_horizontal = np.hstack((gray_copy, seg[:, :, 0], seg[:, :, 1], seg[:, :, 2], maximums))
     # cv.imshow('Numpy Horizontal', numpy_horizontal)
-    # # cv2.imshow('Numpy Vertical Concat', numpy_vertical_concat)
-    # # cv2.imshow('Numpy Horizontal Concat', numpy_horizontal_concat)
-    #
-    # # cv.imshow("siva", cv.cvtColor(rgb_new, cv.COLOR_BGR2GRAY))
+    # cv2.imshow('Numpy Vertical Concat', numpy_vertical_concat)
+    # cv2.imshow('Numpy Horizontal Concat', numpy_horizontal_concat)
+
+    # cv.imshow("siva", cv.cvtColor(rgb_new, cv.COLOR_BGR2GRAY))
     # prev_gray = gray
     # if cv.waitKey(0) & 0xFF == ord('q'):
     #     break
-
 
 ######################################
 # puno zelenih točaka
@@ -281,7 +443,6 @@ for i in range(0, len(images), temporal_length):
 #     ch = cv.waitKey(1)
 #     if ch == 27:
 #         break
-
 
 
 # path = os.path.join('data', 'UCSD_Anomaly_Dataset.v1p2', 'UCSDped1', 'Train', 'Train001')

2a270f0222659f332e1003ba37c2eb8604aed6e3
kuzzmak <ak50380@fer.hr>
2020-12-29 00:58:32 +0100

    updated requirements, added function for converting SVOI to tensor for cnn input

--- util.py
+++ util.py
@@ -1,6 +1,8 @@
 import cv2 as cv
 import numpy as np
 
+import torch
+
 import os
 
 
@@ -87,3 +89,52 @@ def get_image_paths(folder_path: str, ext: str) -> list:
     image_names = [x for x in os.listdir(folder_path) if x.endswith(ext)]
     image_paths = [os.path.join(folder_path, x) for x in image_names]
     return image_paths
+
+
+def resize_svoi(svoi: np.ndarray, preferred_square_size: tuple) -> np.ndarray:
+    """
+    Resizes all squares in one SVOI.
+
+    Parameters
+    ----------
+    svoi: np.ndarray
+        SVOI which squares are being resized
+    preferred_square_size: tuple
+        new size of squares in SVOI
+
+    Returns
+    -------
+    resized_svoi: np.ndarray
+        new SVOI with resized squares
+    """
+
+    assert len(preferred_square_size) == 2, "square size in SVOI must be two dimensional"
+    new_shape = (svoi.shape[0], preferred_square_size[0], preferred_square_size[1])
+    resized_svoi = np.zeros(new_shape, dtype='uint8')
+    for dim in range(svoi.shape[0]):
+        resized_svoi[dim, :, :] = resize_image(svoi[dim, :, :], preferred_square_size)
+    return resized_svoi
+
+
+def make_tensor_from_svoi(svoi: np.ndarray) -> torch.tensor:
+    """
+    Converts numpy array of shape (temporal_length, x, y) to (batch_size, temporal_length, depth, x, x),
+    where batch_size if number of SVOI samples for iteration (1 here) and depth is how many colors
+    does one square have (1 here, only grayscale images).
+    After conversion, torch.tensor because it's convenient for input into CNN.
+
+    Parameters
+    ----------
+    svoi: np.ndarray
+        SVOI
+    Returns
+    -------
+    input_tensor: torch.tensor
+        tensor for input into CNN
+
+    """
+    # 5D: [batch_size, channels, depth, height, width]
+    new_svoi = np.zeros((1, 1, *svoi.shape), dtype=svoi.dtype)
+    for dim in range(svoi.shape[0]):
+        new_svoi[0, 0, dim, :, :] = svoi[dim, :, :]
+    return torch.from_numpy(new_svoi.astype(np.float32))

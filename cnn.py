1a5ffa5388dec204648fe8e88e80425493607c86
kuzzmak <ak50380@fer.hr>
2021-01-06 00:59:57 +0100

    implemented learning

--- cnn.py
+++ cnn.py
@@ -1,63 +1,73 @@
-import torch
-import torch.nn as nn
+import os
 
-import numpy as np
+import torch
+from torch.autograd import Variable
 
 import util
-from model import NetModel
 from svoidataset import SVOIDataset
 
 
 class CNN:
 
-    def __init__(self, model, optimizer, criterion, dataset_params):
+    def __init__(self, model, optimizer, criterion, save_model, dataset_params):
         self.model = model
         self.optimizer = optimizer
         self.criterion = criterion
+        self.save_model = save_model
         self.dataset_params = dataset_params
+
+    @staticmethod
+    def normalize_cnn_output(output):
+        """
+        Normalizes values that come out of the cnn in a way that
+        they represent probability of each class and sum to 1.
+
+        Parameters
+        ----------
+        output: torch.tensor
+            output of the cnn
+
+        Returns
+        -------
+        output: torch.tensor
+            probabilities of each class
+        """
+
+        out = output.data[0]
+        p1 = 1 / (1 + torch.exp(out[1] - out[0]))
+        p2 = 1 / (1 + torch.exp(out[0] - out[1]))
+        return torch.tensor([[p1, p2]], dtype=torch.float32)
 
     def train(self, epochs):
         self.model.train()
+        print('training started...')
 
         for epoch in range(epochs):
 
             sd = SVOIDataset(self.dataset_params)
             for s, labels in sd:
 
-                target = int(1 in labels)
+                target = torch.tensor([int(1 in labels)], dtype=torch.long)
 
                 for square, svoi in s.items():
                     resized_svoi = util.resize_svoi(svoi, (32, 32))
                     svoi_tensor = util.make_tensor_from_svoi(resized_svoi)
 
-                    # TODO fix this
-                    # output = self.model(svoi_tensor)
-                    # probs = util.get_probabilities_from_cnn_output(output.data[0])
-                    # output = torch.tensor([[np.argmax(probs)]], dtype=torch.float32)
-                    # output = np.argmax(probs)
-                    # loss = self.criterion(output, target)
-                    #
-                    # self.optimizer.zero_grad()
-                    # loss.backward()
-                    # self.optimizer.step()
+                    output = self.model(svoi_tensor)
+                    output = self.normalize_cnn_output(output)
 
-            print(f'epoch: {epoch}')
+                    loss = self.criterion(output, target)
+                    loss = Variable(loss, requires_grad=True)
 
+                    self.optimizer.zero_grad()
+                    loss.backward()
+                    self.optimizer.step()
 
-if __name__ == "__main__":
-    _model = NetModel()
+            print(f'\tepoch: {epoch + 1}')
+        print('training finished')
 
-    _optimizer = torch.optim.SGD(_model.parameters(), lr=0.1)
-    _criterion = nn.CrossEntropyLoss()
-
-    _dataset_params_ucsd = dict(
-        dataset=util.UCSD,
-        name=util.PED1,
-        test_num=1,
-        temporal_length=7,
-        ext='.tif',
-    )
-    cnn = CNN(_model, _optimizer, _criterion, _dataset_params_ucsd)
-
-    _epochs = 100
-    cnn.train(_epochs)
+        if self.save_model:
+            print('saving model')
+            model_path = os.path.join('models', 'cnn.pt')
+            torch.save(self, model_path)
+            print('model saved to: ', model_path)

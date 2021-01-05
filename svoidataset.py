79e2646b7ba6d152ad3cfa441979385c5fba1886
kuzzmak <ak50380@fer.hr>
2021-01-05 01:30:54 +0100

    refactored

--- svoidataset.py
+++ svoidataset.py
@@ -1,4 +1,5 @@
-from typing import Iterator
+from typing import Iterator, Iterable
+import os
 
 from torch.utils.data import IterableDataset
 
@@ -8,43 +9,52 @@ from svoi import SVOI
 
 class SVOIDataset(IterableDataset):
 
-    def __init__(self, dataset_path: str, ext: str, temporal_length: int, dataset_params: dict):
+    def __init__(self, dataset_params: dict):
         """
         Class representing iterable dataset where each iteration consists of SVOI
         and corresponding labels for each frame of SVOI.
-
-        How to use
-        ----------
-        dataset_path = os.path.join(util.UCSD_PATH, 'ped1', 'Test', 'Test001')
-        sd = SVOIDataset(dataset_path, '.tif', 7, dataset_params)
-        for svoi, labels in sd:
-            do something...
-
-        Parameters
-        ----------
-        dataset_path: str
-            path to dataset folder with pictures
-        ext: str
-            extension of images in dataset
-        temporal_length: int
-            length of the SVOI in temporal domain
-        dataset_params: dict
-            parameters for dataset
         """
 
-        self.image_paths = util.get_image_paths(dataset_path, ext)
-        self.temporal_length = temporal_length
         self.dataset_params = dataset_params
+        self.image_paths = self.get_image_paths(dataset_params)
         self.svoi = SVOI(self.image_paths)
 
     def __iter__(self) -> Iterator:
         return zip(
             self.svoi.generator(),
-            util.labels_generator(
-                self.temporal_length,
-                util.get_labels_ucsd,
-                self.dataset_params)
+            util.labels_generator(self.dataset_params)
         )
 
     def __getitem__(self, index):
         pass
+
+    @staticmethod
+    def get_image_paths(dataset_params: dict) -> Iterable:
+
+        dataset = dataset_params['dataset']
+
+        dataset_name = dataset_params['name']
+        ext = dataset_params['ext']
+        if dataset == util.UCSD:
+            if dataset_name == util.PED1:
+                folder_path = util.UCSD_PED1_PATH
+            else:
+                folder_path = util.UCSD_PED2_PATH
+
+            test_num = dataset_params.get('test_num', 1)
+            frames_folder = os.path.join(folder_path, 'Test', 'Test{:03d}'.format(test_num))
+
+        else:
+            # UMN dataset
+            if dataset_name == util.INDOOR:
+                folder_path = util.UMN_INDOOR_PATH
+            elif dataset_name == util.LAWN:
+                folder_path = util.UMN_LAWN_PATH
+            else:
+                folder_path = util.UMN_PLAZA_PATH
+
+            frames_folder = os.path.join(folder_path, 'frames')
+
+        image_paths = util.get_image_paths(frames_folder, ext)
+
+        return image_paths
